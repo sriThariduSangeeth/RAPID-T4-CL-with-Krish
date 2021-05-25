1b042bafb5d1b904ff81d89c4f670db22c908315
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-25 18:23:25 +0530

    selection sort java impl

--- Algorithms-and-Data-Structures/Sorting-Techniques/Selection-Sort/Selection-Sort-Algorithm-Java/src/com/rapid/sangeeth/main/Main.java
+++ Algorithms-and-Data-Structures/Sorting-Techniques/Selection-Sort/Selection-Sort-Algorithm-Java/src/com/rapid/sangeeth/main/Main.java
@@ -1,9 +1,25 @@
 package com.rapid.sangeeth.main;
 
+import com.rapid.sangeeth.arraySort.SelectionSort;
+
 /**
  * @author dtsangeeth
  * @created 25 / 05 / 2021
  * @project Selection-Sort-Algorithm-Java
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.SortIntArry();
+
+    }
+
+    private void SortIntArry(){
+        SelectionSort ob = new SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
 }
