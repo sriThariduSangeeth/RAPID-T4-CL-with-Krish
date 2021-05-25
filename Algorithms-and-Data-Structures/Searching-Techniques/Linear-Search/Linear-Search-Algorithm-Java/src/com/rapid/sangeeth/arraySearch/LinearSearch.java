0d6b8018807cab8c6bc48149cc0d1ed60fb0e453
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-25 20:36:44 +0530

    linear search java

---
+++ Algorithms-and-Data-Structures/Searching-Techniques/Linear-Search/Linear-Search-Algorithm-Java/src/com/rapid/sangeeth/arraySearch/LinearSearch.java
@@ -0,0 +1,9 @@
+package com.rapid.sangeeth.arraySearch;
+
+/**
+ * @author dtsangeeth
+ * @created 25 / 05 / 2021
+ * @project Linear-Search-Algorithm-Java
+ */
+public class LinearSearch {
+}
---
+++ Algorithms-and-Data-Structures/Searching-Techniques/Linear-Search/Linear-Search-Algorithm-Java/src/com/rapid/sangeeth/main/Main.java
@@ -0,0 +1,13 @@
+package com.rapid.sangeeth.main;
+
+/**
+ * @author dtsangeeth
+ * @created 25 / 05 / 2021
+ * @project Linear-Search-Algorithm-Java
+ */
+public class Main{
+
+    public static void main(String[] args) {
+
+    }
+}
