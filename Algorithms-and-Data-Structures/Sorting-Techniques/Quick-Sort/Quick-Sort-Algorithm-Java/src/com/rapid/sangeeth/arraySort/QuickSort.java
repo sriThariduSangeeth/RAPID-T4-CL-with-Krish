38cdca33b6df9c182447db700b09812f05f63dd2
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-25 17:30:29 +0530

    quick sort java impl

---
+++ Algorithms-and-Data-Structures/Sorting-Techniques/Quick-Sort/Quick-Sort-Algorithm-Java/src/com/rapid/sangeeth/arraySort/QuickSort.java
@@ -0,0 +1,62 @@
+package com.rapid.sangeeth.arraySort;
+
+/**
+ * @author dtsangeeth
+ * @created 25 / 05 / 2021
+ * @project Quick-Sort-Algorithm-Java
+ */
+public class QuickSort {
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public int partition(int[] arr, int low, int high) {
+
+        // pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+
+        for(int j = low; j <= high - 1; j++) {
+
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot)
+            {
+
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void printArray(int[] arr, int size) {
+        for(int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+}
--- Algorithms-and-Data-Structures/Sorting-Techniques/Quick-Sort/Quick-Sort-Algorithm-Java/src/com/rapid/sangeeth/main/Main.java
+++ Algorithms-and-Data-Structures/Sorting-Techniques/Quick-Sort/Quick-Sort-Algorithm-Java/src/com/rapid/sangeeth/main/Main.java
@@ -1,9 +1,27 @@
 package com.rapid.sangeeth.main;
 
+import com.rapid.sangeeth.arraySort.QuickSort;
+
 /**
  * @author dtsangeeth
  * @created 25 / 05 / 2021
  * @project Quick-Sort-Algorithm-Java
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.sortIntArray();
+    }
+
+    private void sortIntArray(){
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+
+        QuickSort ob = new QuickSort();
+        ob.quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        ob.printArray(arr, n);
+    }
+
 }
