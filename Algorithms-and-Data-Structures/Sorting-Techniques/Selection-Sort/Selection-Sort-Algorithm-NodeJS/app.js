cb92a8532577829f8f9ab2b1a82239ad1e762a1e
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-25 18:19:22 +0530

    selection sort node impl

---
+++ Algorithms-and-Data-Structures/Sorting-Techniques/Selection-Sort/Selection-Sort-Algorithm-NodeJS/app.js
@@ -0,0 +1,21 @@
+function swap(a, b, arr) {
+    let tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+function selectionSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[min] > array[j]) min = j;
+        }
+        swap(i, min, array);
+    }
+    return array;
+}
+
+// Using Taditional array to swap
+var intarrayone = new Array(1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92);
+selectionSort(intarrayone);
+console.log(intarrayone);
