ed053350755d53bd6dba68ae25b5b657b51e8dec
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-27 17:25:33 +0530

    anagram

--- Practical-Tasks-01/api/qFour/rapid.qFour.service.js
+++ Practical-Tasks-01/api/qFour/rapid.qFour.service.js
@@ -1,7 +1,40 @@
-function queationFour(res) {
+function queationFour(req) {
     return new Promise((resolve, reject) => {
-        resolve("this method call");
+        if (req.body) {
+            try {
+                let first = req.body.inputOne;
+                let second = req.body.inputTwo;
+                resolve(findAnagram(first, second));
+            } catch (e) {
+                reject(e);
+            }
+        } else {
+            reject("reqest body is not empty");
+        }
     });
 };
+
+const findAnagram = (val1, val2) => {
+    const reverseString = reversArry(val1.toLowerCase());
+    if (reverseString == val2.toLowerCase()) {
+        return "True... Yes These two words are anagram";
+    } else {
+        return "False... No These two words are not anagram";
+    }
+}
+
+const reversArry = (arr) => {
+    var ret = new Array;
+    var res = "";
+    const stringArr = Array.from(arr.replace(/ /g, ''));
+
+    // to reverse string array
+    for (var i = stringArr.length - 1; i >= 0; i--) {
+        ret.push(stringArr[i]);
+        res += stringArr[i] + "";
+    }
+    console.log(res);
+    return res;
+}
 
 module.exports = { queationFour };
