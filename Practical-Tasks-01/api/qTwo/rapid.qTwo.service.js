27a10e78d85cb2e71d2694e8ea730f8972948c00
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-05-27 16:17:44 +0530

    count duplicateds

--- Practical-Tasks-01/api/qTwo/rapid.qTwo.service.js
+++ Practical-Tasks-01/api/qTwo/rapid.qTwo.service.js
@@ -1,7 +1,44 @@
-function queationTwo(res) {
+function queationTwo(req) {
     return new Promise((resolve, reject) => {
-        resolve("this method call");
+        if (req.body.word) {
+            try {
+                resolve(findRepetition(req.body.word));
+            } catch (e) {
+                reject(e);
+            }
+        } else {
+            reject("reqest body is not empty");
+        }
     });
+};
+
+const findRepetition = (word) => {
+
+    const stringArr = Array.from(word.replace(/ /g, ''));
+    // sort string array
+    stringArr.sort();
+    var count = {};
+    // insert into map
+    stringArr.forEach(function(i) { count[i] = (count[i] || 0) + 1; });
+    return count;
+};
+
+
+//to remove duplicate in array
+//there are three methods to use
+
+const removeDuplicates = (word) => {
+
+    // Step 1
+    const uniqueSet = new Set(word);
+    const backToArray = [...uniqueSet];
+
+    // 2: "Filter"
+    word.filter((item, index) => word.indexOf(item) === index);
+    // 3: "Reduce"
+    word.reduce(
+        (unique, item) => (unique.includes(item) ? unique : [...unique, item]), [],
+    );
 };
 
 module.exports = { queationTwo };
