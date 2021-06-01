78f18a33a1a9c6113ffe0e6dfe529b9f5c8077c6
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-06-01 19:25:51 +0530

    increase array and find Height

--- binary-search-tree/print-all-leaf-nodes/binary-tree.js
+++ binary-search-tree/print-all-leaf-nodes/binary-tree.js
@@ -80,6 +80,31 @@ class BinarySearchTree {
         }
         return current.value;
     }
+
+    findMinHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        };
+        let left = this.findMinHeight(node.left);
+        let right = this.findMinHeight(node.right);
+        if (left < right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
+    findMaxHeight(node = this.root) {
+        if (node == null) {
+            return -1;
+        };
+        let left = this.findMaxHeight(node.left);
+        let right = this.findMaxHeight(node.right);
+        if (left > right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
 }
 
 module.exports = BinarySearchTree;
--- binary-search-tree/print-all-leaf-nodes/main.js
+++ binary-search-tree/print-all-leaf-nodes/main.js
@@ -2,18 +2,27 @@ const BinarySearchTree = require("./binary-tree");
 
 
 const binarytree = new BinarySearchTree();
-binarytree.push(52);
-binarytree.push(15);
-binarytree.push(56);
-binarytree.push(9);
-binarytree.push(11);
-binarytree.push(54);
-binarytree.push(3);
-binarytree.push(5);
-binarytree.push(61);
+// binarytree.push(52);
+// binarytree.push(15);
+// binarytree.push(56);
+// binarytree.push(9);
+// binarytree.push(11);
+// binarytree.push(54);
+// binarytree.push(3);
+// binarytree.push(5);
+// binarytree.push(61);
 
+const numberArray = [218, 99, 57, 80, 37, 243, 162, 141, 188, 54, 143, 243, 137, 50, 42, 52, 15, 56, 9, 11, 54, 3, 5, 61];
+
+numberArray.forEach(number => {
+    binarytree.push(number);
+});
 console.log("Leaf Nodes in tree : ", binarytree.displayLeafNodes());
 
 console.log("Print Minmum vlaue in tree : ", binarytree.findMinValue());
 
 console.log("Print Maximum vlaue in tree : ", binarytree.findMaxValue());
+
+console.log("Print Maximum Height in tree : ", binarytree.findMaxHeight());
+
+console.log("Print Minmum Height in tree : ", binarytree.findMinHeight());
