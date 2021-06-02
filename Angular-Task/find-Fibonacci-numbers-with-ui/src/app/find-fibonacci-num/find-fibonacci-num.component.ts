01031c116be9f7032c3d01785a524148db6566ed
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-06-03 03:27:51 +0530

    solve array

--- Angular-Task/find-Fibonacci-numbers-with-ui/src/app/find-fibonacci-num/find-fibonacci-num.component.ts
+++ Angular-Task/find-Fibonacci-numbers-with-ui/src/app/find-fibonacci-num/find-fibonacci-num.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class FindFibonacciNumComponent implements OnInit {
 
 
+  arrFirst: number = 0;
+  arrLast: number = 0;
   fibNumber: number = 0;
   fibIndex: number = 0;
 
   outFibNumber: any = 0;
+  outFibIndex: any = 0;
+  outFibMAp = new Array<number>();
+  indexRes: boolean = true;
 
   constructor() { }
 
@@ -19,8 +24,9 @@ export class FindFibonacciNumComponent implements OnInit {
   }
 
   genarateSequence(first: string, last: string) {
-    console.log(first + "hello" + last);
-
+    this.arrFirst = +first;
+    this.arrLast = +last;
+    this.outFibMAp.push(...this.calculateArrayOfFibonacci(this.arrFirst, this.arrLast));
   }
 
   calFibonacci() {
@@ -34,21 +40,86 @@ export class FindFibonacciNumComponent implements OnInit {
   }
 
   calIndexOfFibonacci() {
-    console.log("hello2" + this.fibIndex);
+    if (this.isFibonacci(this.fibIndex)) {
+      console.log(this.isFibonacci(this.fibIndex));
+      this.indexRes = true;
+      this.outFibIndex = this.calculateIndexOfFibonacci(this.fibIndex);
+    } else {
+      this.indexRes = false;
+      this.outFibIndex = "Invalid input. This is not Fibonacci Nmber";
+    }
+
+
   }
 
   calculateFibonacci(intput: number): number {
 
-    let fibonacciArry = new Array(intput + 2);
-    fibonacciArry[0] = 0;
-    fibonacciArry[1] = 1;
-    for (let i = 2; i <= intput; i++) {
-      fibonacciArry[i] = fibonacciArry[i - 1] + fibonacciArry[i - 2];
+    // let fibonacciArry = new Array(intput + 2);
+    // fibonacciArry[0] = 0;
+    // fibonacciArry[1] = 1;
+    // for (let i = 2; i <= intput; i++) {
+    //   console.log(fibonacciArry[i]);
+
+    //   fibonacciArry[i] = fibonacciArry[i - 1] + fibonacciArry[i - 2];
+    //   console.log(fibonacciArry[i]);
+    // }
+    // return fibonacciArry[intput];
+
+    if (intput <= 1)
+      return intput;
+
+    return this.calculateFibonacci(intput - 1) + this.calculateFibonacci(intput - 2);
+  }
+
+  calculateIndexOfFibonacci(input: number): number {
+
+    let result = 1;
+    let fib1 = 0;
+    let fib2 = 1;
+    let fib3 = 1;
+
+    if (this.fibIndex <= 1) {
+      return this.fibIndex;
     }
-    return fibonacciArry[intput];
+
+    while (fib3 < input) {
+      fib3 = fib1 + fib2;
+      result++;
+      fib1 = fib2;
+      fib2 = fib3;
+    }
+    return result;
   }
 
-  calculateArrayOfFibonacci(first: number, last: number) {
-
+  isFibonacci(input: number) {
+    let firstTerm = 0;
+    let secondTerm = 1;
+    let thirdTerm = 0;
+    while (thirdTerm < input) {
+      thirdTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = thirdTerm;
+    }
+    if (thirdTerm == input) {
+      return true;
+    }
+    return false;
   }
+
+  /**
+   * //TODO
+   * @param first 
+   * @param last 
+   * @returns 
+   */
+  calculateArrayOfFibonacci(first: number, last: number): number[] {
+    let fibonacciArry = new Array();
+
+    for (let i = first; i <= last; i++) {
+      fibonacciArry.push(this.calculateFibonacci(i));
+    }
+    return fibonacciArry;
+  }
+
+
 }
